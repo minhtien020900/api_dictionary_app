fd6e8912900bd2faf3c85ef8e0e82e0a60c21fd6
minhtien020900 <minhtien020900@gmail.com>
2021-07-30 14:31:30 +0700

    Fix response register message

--- config/api_custom.php
+++ config/api_custom.php
@@ -5,6 +5,7 @@ use Symfony\Component\HttpFoundation\Response;
 return [
     'no_have_error' => env('NO_HAVE_ERROR', 0),
     'have_error' => env('HAVE_ERROR', 1),
+    'no_have_error_msg' => env('NO_HAVE_ERROR_MSG', 0),
     'status_code' => [
         422 => Response::HTTP_UNPROCESSABLE_ENTITY,
         401 => Response::HTTP_UNAUTHORIZED,
